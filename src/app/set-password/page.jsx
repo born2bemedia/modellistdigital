85906f74ff33e43d3e57c49456d2d496b2eb647b
SergWebAcceptance <serg.web@acceptance.ltd>
2024-07-17 17:33:33 +0300

    fixed set password page

--- src/app/set-password/page.jsx
+++ src/app/set-password/page.jsx
@@ -1,22 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import ChangePasswordReset from "../(account)/dashboard/_components/ChangePasswordReset";
 
 const SetPassword = () => {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
   const token = searchParams.get("token");
-  const router = useRouter();
+  const [isValid, setIsValid] = useState(false);
 
   useEffect(() => {
     if (email && token) {
       localStorage.setItem("resetToken", token);
       localStorage.setItem("resetEmail", email);
+      setIsValid(true);
+    } else {
+      setIsValid(false);
     }
   }, [email, token]);
 
-  if (!email || !token) {
+  if (!isValid) {
     return <div>Invalid or expired link.</div>;
   }
 
