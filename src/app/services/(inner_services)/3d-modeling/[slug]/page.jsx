1cba6373bf0ddadce23715675f3ef427ef21937b
SergWebAcceptance <serg.web@acceptance.ltd>
2024-07-24 11:46:25 +0300

    Added 3d models

--- src/app/services/(inner_services)/3d-modeling/[slug]/page.jsx
+++ src/app/services/(inner_services)/3d-modeling/[slug]/page.jsx
@@ -1,52 +1,24 @@
-"use client";
 import "@/styles/services.scss";
 import { fetchProductsByCategory } from "@/app/api/products";
-import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { fadeInUp } from "@/utils/animations";
 import Link from "next/link";
 import AddToCartButtonProduct from "@/components/AddToCartButtonProduct";
 
-const ModelingCategory = ({ params: { slug } }) => {
-  const [products, setProducts] = useState([]);
-  const [title, setTitle] = useState();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const pageTitle = slug.replace("-", " ");
-    setTitle(pageTitle);
-
-    const fetchProducts = async () => {
-      try {
-        const data = await fetchProductsByCategory(slug, 9999);
-        setProducts(data);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, [slug]);
+const ModelingCategory = async ({ params: { slug } }) => {
+  const pageTitle = slug.replace("-", " ");
+  const products = await fetchProductsByCategory(slug, 9999);
 
   return (
     <>
       <section className="category-hero">
         <div className="_container">
           <div className="category-hero__body">
-            <motion.img
-              initial="hidden"
-              animate="visible"
-              variants={fadeInUp}
+            <img
               src="/images/services/3d/3dHeroIcon.svg"
               width={48}
               height={48}
-            ></motion.img>
-            <motion.h1 initial="hidden" animate="visible" variants={fadeInUp}>
-              {title}
-            </motion.h1>
+              alt="3D Hero Icon"
+            />
+            <h1>{pageTitle}</h1>
           </div>
         </div>
       </section>
@@ -55,39 +27,40 @@ const ModelingCategory = ({ params: { slug } }) => {
         <div className="_container">
           <div className="products__body">
             {products.map((product) => (
-              <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                variants={fadeInUp}
-                key={product.id}
-              >
+              <div key={product.id}>
                 <Link
                   className="product-thumb"
                   href={`/product/${product.slug}`}
                 >
-                  <img src={product.image} width={350} height={197} />
+                  <img
+                    src={product.image}
+                    width={350}
+                    height={197}
+                    alt={product.title}
+                  />
                   {product.offer && (
                     <span className="offer">{product.offer} OFF!</span>
                   )}
                 </Link>
 
                 <div className="product-info">
-                  <h3>
-                    <Link href={`/product/${product.slug}`}>
-                      {product.title}
-                    </Link>
-                  </h3>
-                  <div
-                    className="excerpt"
-                    dangerouslySetInnerHTML={{ __html: product.excerpt }}
-                  />
+                  <div>
+                    <h3>
+                      <Link href={`/product/${product.slug}`}>
+                        {product.title}
+                      </Link>
+                    </h3>
+                    <div
+                      className="excerpt"
+                      dangerouslySetInnerHTML={{ __html: product.excerpt }}
+                    />
+                  </div>
                   <div className="add-to-cart">
                     <span>Price: €{product.price}</span>
                     <AddToCartButtonProduct product={product} />
                   </div>
                 </div>
-              </motion.div>
+              </div>
             ))}
           </div>
         </div>
--- src/app/services/(inner_services)/3d-modeling/_components/Products.jsx
+++ src/app/services/(inner_services)/3d-modeling/_components/Products.jsx
@@ -1,129 +1,58 @@
-"use client";
-import React, { Suspense, useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import { fadeInUp } from "@/utils/animations";
 import Link from "next/link";
-import AddToCartButton from "@/components/AddToCartButton";
 import { fetchProductsByCategory } from "@/app/api/products";
 import AddToCartButtonProduct from "@/components/AddToCartButtonProduct";
 import AddToCartArrow from "@/icons/AddToCartArrow";
-import Video from "next-video";
-import VideoBlock from "@/app/product/[slug]/_components/Video";
 
-const Products = ({ category, title }) => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const data = await fetchProductsByCategory(category, 3);
-        setProducts(data);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, [category]);
+const Products = async ({ category, title }) => {
+  const products = await fetchProductsByCategory(category, 3);
 
   return (
     <>
-      <section className="products">
-        <div className="_container">
-          {title && (
-            <motion.h2
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              variants={fadeInUp}
-            >
-              {title}
-            </motion.h2>
-          )}
-          <div className="products__body">
-            {products.map((product) => (
-              <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                variants={fadeInUp}
-                key={product.id}
-              >
-                {product.preview_video ? (
-                  <Link
-                    className="product-thumb"
-                    href={`/product/${product.slug}`}
-                  >
-                    <img src={product.image} width={350} height={197} />
-                    {product.offer && (
-                      <span className="offer">{product.offer} OFF!</span>
-                    )}
+      {products.length > 0 && (
+        <section className="products">
+          <div className="_container">
+            {title && <h2>{title}</h2>}
+            <div className="products__body">
+              {products.map((product) => (
+                <div key={product.id}>
+                  <Link className="product-thumb" href={`/product/${product.slug}`}>
+                    <img src={product.image} width={350} height={197} alt={product.title} />
+                    {product.offer && <span className="offer">{product.offer} OFF!</span>}
                   </Link>
-                ) : (
-                  <Link
-                    className="product-thumb"
-                    href={`/product/${product.slug}`}
-                  >
-                    <img src={product.image} width={350} height={197} />
-                    {product.offer && (
-                      <span className="offer">{product.offer} OFF!</span>
-                    )}
-                  </Link>
-                )}
 
-                <div className="product-info">
-                  <h3>
-                    <Link href={`/product/${product.slug}`}>
-                      {product.title}
-                    </Link>
-                  </h3>
-                  <div
-                    className="excerpt"
-                    dangerouslySetInnerHTML={{ __html: product.excerpt }}
-                  />
-                  <div className="add-to-cart">
-                    <span>Price: €{product.price}</span>
-                    {category == "ready-made-animations" ? (
-                      <Link
-                        className="arrow-button"
-                        href={`/product/${product.slug}`}
-                      >
-                        View
-                        <AddToCartArrow />
-                      </Link>
-                    ) : (
-                      <AddToCartButtonProduct product={product} />
-                    )}
+                  <div className="product-info">
+                    <div>
+                      <h3>
+                        <Link href={`/product/${product.slug}`}>{product.title}</Link>
+                      </h3>
+                      <div className="excerpt" dangerouslySetInnerHTML={{ __html: product.excerpt }} />
+                    </div>
+                    <div className="add-to-cart">
+                      <span>Price: €{product.price}</span>
+                      {category === "ready-made-animations" ? (
+                        <Link className="arrow-button" href={`/product/${product.slug}`}>
+                          View
+                          <AddToCartArrow />
+                        </Link>
+                      ) : (
+                        <AddToCartButtonProduct product={product} />
+                      )}
+                    </div>
                   </div>
                 </div>
-              </motion.div>
-            ))}
+              ))}
 
-            {category != "hot-offers" && category != "ready-made-animations" ? (
-              <motion.div
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                variants={fadeInUp}
-                className="button-wrap"
-              >
-                <Link
-                  className="black-button"
-                  href={`/services/3d-modeling/${category}`}
-                >
-                  All {title}
-                </Link>
-              </motion.div>
-            ) : (
-              ""
-            )}
+              {category !== "hot-offers" && category !== "ready-made-animations" && title !== "YOU MAY ALSO LIKE" && (
+                <div className="button-wrap">
+                  <Link className="black-button" href={`/services/3d-modeling/${category}`}>
+                    All {title}
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 };
