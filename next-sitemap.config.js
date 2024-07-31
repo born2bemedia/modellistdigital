d1c0d25502930ad9fcb3a9ec3cb3cc048fb0e9c3
SergWebAcceptance <serg.web@acceptance.ltd>
2024-07-31 12:20:44 +0300

    Fixed sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,4 @@ module.exports = {
   siteUrl: "https://modellistdigital.com",
   generateRobotsTxt: true,
   exclude: ["/sitemap.xml"],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://modellistdigital.com/sitemap.xml"],
-  },
 };
