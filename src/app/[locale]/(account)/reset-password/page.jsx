78da0bc491e33f27966d9d0559790db9a787d748
daniil-b2be <dan.developer@born2be.media>
2025-12-02 22:16:03 +0200

    reset password translations

--- src/app/[locale]/(account)/reset-password/page.jsx
+++ src/app/[locale]/(account)/reset-password/page.jsx
@@ -3,16 +3,19 @@ import "@/styles/account.scss";
 import { useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { useTranslations } from "next-intl";
 
 export default function PasswordResetRequest() {
+  const t = useTranslations('resetPassword');
+
   const initialValues = {
     email: "",
   };
 
   const validationSchema = Yup.object().shape({
     email: Yup.string()
-      .email("Invalid email address")
-      .required("Email is required"),
+      .email(t('errors.invalidEmail', {fallback: 'Invalid email address'}))
+      .required(t('errors.email', {fallback: 'Email is required'})),
   });
 
   const handleSubmit = async (
@@ -43,7 +46,7 @@ export default function PasswordResetRequest() {
   return (
     <section className="log-in">
       <div className="_container">
-        <h1>Reset your password</h1>
+        <h1>{t('title', {fallback: 'Reset your password'})}</h1>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -55,7 +58,7 @@ export default function PasswordResetRequest() {
                 <Field
                   type="email"
                   name="email"
-                  placeholder="Email"
+                  placeholder={t('fields.email', {fallback: 'Email'})}
                   className={touched.email && errors.email ? "invalid" : ""}
                 />
                 <ErrorMessage name="email" component="div" className="error" />
@@ -65,11 +68,11 @@ export default function PasswordResetRequest() {
                 type="submit"
                 disabled={isSubmitting}
               >
-                Reset
+                {t('button', {fallback: 'Reset'})}
               </button>
               {status && status.success && (
                 <div className="success">
-                  <p>Reset link was sent to your email!</p>
+                  <p>{t('success.title', {fallback: 'Reset link was sent to your email!'})}</p>
                 </div>
               )}
             </Form>
